db65e7d6b5704c353b2f9d933fb0fe1fa6058faf
DanFilipe2000 <anielilipe90@gmail.com>
2023-01-13 13:29:02 -0300

    fix: try add withStatus(200)

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -22,7 +22,7 @@ class ContactController extends Controller
     // Cria o registro de um contato no banco de dados:
 
     public function create() {
-        return view('contact.create');
+        return view('contact.create')->withStatus(200);
     }
 
     public function store (Request $request) {
@@ -36,7 +36,7 @@ class ContactController extends Controller
             'user_id' => $user->id,
         ]);
 
-        return view('contact.sucess');
+        return redirect('/contacts');
     }
 
     // Recupera um contato baseado no id:
